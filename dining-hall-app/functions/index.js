3db721d6624f456667f185808819ba6a4b1333e9
Ryan Sullivan <ryan@MacBook-Air-402.local>
2024-12-05 17:21:59 -0600

    Fixing checkout logic for location

--- dining-hall-app/functions/index.js
+++ dining-hall-app/functions/index.js
@@ -4,52 +4,56 @@ admin.initializeApp();
 
 const firestore = admin.firestore();
 
+exports.scheduleCheckOut = () => {
+  console.log("Scheduled check-out function called.");
+}
+
 // Cloud Function to delete expired check-ins
-exports.scheduledCheckOut = functions.pubsub.schedule("every 1 hours")
-    .onRun(async () => {
-      const now = admin.firestore.Timestamp.now();
-      const oneHourAgo = new admin.firestore
-          .Timestamp(now.seconds - 3600, now.nanoseconds);
+// exports.scheduledCheckOut = functions.pubsub.schedule("every 1 hours")
+//     .onRun(async () => {
+//       const now = admin.firestore.Timestamp.now();
+//       const oneHourAgo = new admin.firestore
+//           .Timestamp(now.seconds - 3600, now.nanoseconds);
 
-      try {
-        // Query locationReports for expired check-ins
-        const expiredReports = await firestore.collection("locationReports")
-            .where("timestamp", "<=", oneHourAgo)
-            .get();
+//       try {
+//         // Query locationReports for expired check-ins
+//         const expiredReports = await firestore.collection("locationReports")
+//             .where("timestamp", "<=", oneHourAgo)
+//             .get();
 
-        // const batch = firestore.batch();
+//         // const batch = firestore.batch();
 
-        // expiredReports.forEach((doc) => {
-        //   const diningHallId = doc.data().diningHall;
-        //   const userEmail = doc.data().userEmail;
+//         // expiredReports.forEach((doc) => {
+//         //   const diningHallId = doc.data().diningHall;
+//         //   const userEmail = doc.data().userEmail;
 
-        //   // Decrement occupancy for the respective dining hall
-        //   const diningHallRef = firestore.collection("diningHalls")
-        //       .doc(diningHallId);
-        //   batch.update(diningHallRef, {
-        //     occupancy: admin.firestore.FieldValue.increment(-1),
-        //   });
+//         //   // Decrement occupancy for the respective dining hall
+//         //   const diningHallRef = firestore.collection("diningHalls")
+//         //       .doc(diningHallId);
+//         //   batch.update(diningHallRef, {
+//         //     occupancy: admin.firestore.FieldValue.increment(-1),
+//         //   });
 
-          // // Update user's current location to "none"
-          // const userQuery = firestore.collection("users").where("email", "==", userEmail);
-          // const userSnapshot = await userQuery.get();
-          // if (!userSnapshot.empty) {
-          //     const userDocRef = userSnapshot.docs[0].ref;
-          //     batch.update(userDocRef, {
-          //         currentLocation: "none",
-          //     });
-          // } else {
-          //     console.error("User not found with the given email");
-          // }
+//           // // Update user's current location to "none"
+//           // const userQuery = firestore.collection("users").where("email", "==", userEmail);
+//           // const userSnapshot = await userQuery.get();
+//           // if (!userSnapshot.empty) {
+//           //     const userDocRef = userSnapshot.docs[0].ref;
+//           //     batch.update(userDocRef, {
+//           //         currentLocation: "none",
+//           //     });
+//           // } else {
+//           //     console.error("User not found with the given email");
+//           // }
           
-          // Delete the expired check-in
-        //   batch.delete(doc.ref);
-        // });
+//           // Delete the expired check-in
+//         //   batch.delete(doc.ref);
+//         // });
 
-        // await batch.commit();
-        console.log(`${expiredReports.size} expired check-ins removed.`);
-      } catch (error) {
-        console.error("Error during scheduled checkout:", error);
-      }
-      return null;
-    });
+//         // await batch.commit();
+//         console.log(`${expiredReports.size} expired check-ins removed.`);
+//       } catch (error) {
+//         console.error("Error during scheduled checkout:", error);
+//       }
+//       return null;
+//     });
